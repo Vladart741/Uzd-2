730147df11250cc015b56907d0b49b05a1c92f95
Vladyslav Palkevič <vladyslav.palkevic@mif.stud.vu.lt>
2018-04-05 11:24:53 +0300

    Finalinis commitas

--- uzd-2/main.cpp
+++ uzd-2/main.cpp
@@ -17,7 +17,7 @@ int main()
 	std::cout << "(4) jei norite nuskatityti egzistuojanti faila pagal PIRMA STRATEGIJA" << std::endl;
 	std::cout << "(5) jei norite nuskatityti egzistuojanti faila pagal ANTRA STRATEGIJA" << std::endl;
 
-	pradzia:
+pradzia:
 	std::string ats;
 	std::cin >> ats;
 	if (ats == "2")
@@ -103,9 +103,9 @@ int main()
 	}
 	else if (ats == "4")
 	{
-		std::cout << "Jei norite nuskaityti 1 faila, iveskite (1) " << std::endl;
-		std::cout << "Jei norite nuskaityti visus failus, iveskite (5) " << std::endl;
-		taskas:
+		std::cout << "Jei norite nuskaityti 1 faila naudojant algoritmus, iveskite (1) " << std::endl;
+		std::cout << "Jei norite nuskaityti 1 faila be algoritmu, iveskite (2) " << std::endl;
+	taskas:
 		std::cin >> ats;
 		if (ats == "1")
 		{
@@ -138,144 +138,49 @@ int main()
 
 			system("pause");
 		}
-		else if (ats == "5")
+		else if (ats == "2")
 		{
-			{std::ofstream fr("VEKTORIAI_REZULTATAI.txt");
+			std::cout << "Iveskite failo pavadinima: ";
+			std::cin >> pav;
+			/////////////////////////////////////////////////////// VEKTORIAI ////////////////////////////////////////////////////////////////////////////////////
 			auto start1 = std::chrono::high_resolution_clock::now();
 
-			InputOutput_VEKTORIUS_Pirma_Strategija("Duomenys1.txt");
+			InputOutput_VEKTORIUS_Pirma_Strategija_BE_ALGORITMU(pav);
 
 			auto finish1 = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> elapsed1 = finish1 - start1;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
+			std::cout << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
+			/////////////////////////////////////////////////////// LISTAI ////////////////////////////////////////////////////////////////////////////////////
 			auto start2 = std::chrono::high_resolution_clock::now();
 
-			InputOutput_VEKTORIUS_Pirma_Strategija("Duomenys2.txt");
+			InputOutput_LISTAI_Pirma_Strategija_BE_ALGORITMU(pav);
 
 			auto finish2 = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> elapsed2 = finish2 - start2;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed2.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
+			std::cout << "Programos vykdymo laikas su list: " << elapsed2.count() << " s\n";
+			/////////////////////////////////////////////////////// DEKAI ////////////////////////////////////////////////////////////////////////////////////
 			auto start3 = std::chrono::high_resolution_clock::now();
 
-			InputOutput_VEKTORIUS_Pirma_Strategija("Duomenys3.txt");
+			InputOutput_DEKAI_Pirma_Strategija_BE_ALGORITMU(pav);
 
 			auto finish3 = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> elapsed3 = finish3 - start3;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed3.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start4 = std::chrono::high_resolution_clock::now();
+			std::cout << "Programos vykdymo laikas su deque: " << elapsed3.count() << " s\n";
 
-			InputOutput_VEKTORIUS_Pirma_Strategija("Duomenys4.txt");
-
-			auto finish4 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed4 = finish4 - start4;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed4.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start5 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_VEKTORIUS_Pirma_Strategija("Duomenys5.txt");
-
-			auto finish5 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed5 = finish5 - start5;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed5.count() << " s\n";
-			} // VEKTORIU REZ
-			{std::ofstream fr("DEKAI_REZULTATAI.txt");
-			auto start1 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Pirma_Strategija("Duomenys1.txt");
-
-			auto finish1 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed1 = finish1 - start1;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start2 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Pirma_Strategija("Duomenys2.txt");
-
-			auto finish2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed2 = finish2 - start2;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed2.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start3 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Pirma_Strategija("Duomenys3.txt");
-
-			auto finish3 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed3 = finish3 - start3;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed3.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start4 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Pirma_Strategija("Duomenys4.txt");
-
-			auto finish4 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed4 = finish4 - start4;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed4.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start5 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Pirma_Strategija("Duomenys5.txt");
-
-			auto finish5 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed5 = finish5 - start5;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed5.count() << " s\n";
-			} // DEKU REZ
-			{std::ofstream fr("LISTAI_REZULTATAI.txt");
-			auto start1 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Pirma_Strategija("Duomenys1.txt");
-
-			auto finish1 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed1 = finish1 - start1;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start2 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Pirma_Strategija("Duomenys2.txt");
-
-			auto finish2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed2 = finish2 - start2;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed2.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start3 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Pirma_Strategija("Duomenys3.txt");
-
-			auto finish3 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed3 = finish3 - start3;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed3.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start4 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Pirma_Strategija("Duomenys4.txt");
-
-			auto finish4 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed4 = finish4 - start4;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed4.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start5 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Pirma_Strategija("Duomenys5.txt");
-
-			auto finish5 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed5 = finish5 - start5;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed5.count() << " s\n";
-			} // LISTU REZ
+			system("pause");
 		}
 		else
 		{
-			std::cout << "SKAICIU (1) Jei norite nuskaityti 1 faila" << std::endl;
-			std::cout << "SKAICIU (5) Jei norite nuskaityti visus failus" << std::endl;
+			std::cout << "SKAICIU (1) Jei norite nuskaityti 1 faila naudojant algorimus" << std::endl;
+			std::cout << "SKAICIU (2) Jei norite nuskaityti 1 faila be algoritmu" << std::endl;
 			goto taskas;
 		}
 	}
 	else if (ats == "5")
 	{
-		std::cout << "Jei norite nuskaityti 1 faila, iveskite (1) "<< std::endl;
-		std::cout << "Jei norite nuskaityti visus failus, iveskite (5) " << std::endl;
-		taskas2:
+		std::cout << "Jei norite nuskaityti 1 faila naudojant algoritmus, iveskite (1) " << std::endl;
+		std::cout << "Jei norite nuskaityti 1 faila be algoritmu, iveskite (2) " << std::endl;
+	taskas2:
 		std::cin >> ats;
 		if (ats == "1")
 		{
@@ -308,136 +213,42 @@ int main()
 
 			system("pause");
 		}
-		else if (ats == "5")
+		else if (ats == "2")
 		{
-			{std::ofstream fr("VEKTORIAI_REZULTATAI.txt");
+			std::cout << "Iveskite failo pavadinima: ";
+			std::cin >> pav;
+			/////////////////////////////////////////////////////// VEKTORIAI ////////////////////////////////////////////////////////////////////////////////////
 			auto start1 = std::chrono::high_resolution_clock::now();
 
-			InputOutput_VEKTORIUS_Antra_Strategija("Duomenys1.txt");
+			InputOutput_VEKTORIUS_Antra_Strategija_BE_ALGORITMU(pav);
 
 			auto finish1 = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> elapsed1 = finish1 - start1;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
+			std::cout << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
+			/////////////////////////////////////////////////////// LISTAI ////////////////////////////////////////////////////////////////////////////////////
 			auto start2 = std::chrono::high_resolution_clock::now();
 
-			InputOutput_VEKTORIUS_Antra_Strategija("Duomenys2.txt");
+			InputOutput_LISTAI_Antra_Strategija_BE_ALGORITMU(pav);
 
 			auto finish2 = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> elapsed2 = finish2 - start2;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed2.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
+			std::cout << "Programos vykdymo laikas su list: " << elapsed2.count() << " s\n";
+			/////////////////////////////////////////////////////// DEKAI ////////////////////////////////////////////////////////////////////////////////////
 			auto start3 = std::chrono::high_resolution_clock::now();
 
-			InputOutput_VEKTORIUS_Antra_Strategija("Duomenys3.txt");
+			InputOutput_DEKAI_Antra_Strategija_BE_ALGORITMU(pav);
 
 			auto finish3 = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> elapsed3 = finish3 - start3;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed3.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start4 = std::chrono::high_resolution_clock::now();
+			std::cout << "Programos vykdymo laikas su deque: " << elapsed3.count() << " s\n";
 
-			InputOutput_VEKTORIUS_Antra_Strategija("Duomenys4.txt");
-
-			auto finish4 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed4 = finish4 - start4;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed4.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start5 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_VEKTORIUS_Antra_Strategija("Duomenys5.txt");
-
-			auto finish5 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed5 = finish5 - start5;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed5.count() << " s\n";
-			} // VEKTORIU REZ
-			{std::ofstream fr("DEKAI_REZULTATAI.txt");
-			auto start1 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Antra_Strategija("Duomenys1.txt");
-
-			auto finish1 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed1 = finish1 - start1;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start2 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Antra_Strategija("Duomenys2.txt");
-
-			auto finish2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed2 = finish2 - start2;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed2.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start3 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Antra_Strategija("Duomenys3.txt");
-
-			auto finish3 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed3 = finish3 - start3;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed3.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start4 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Antra_Strategija("Duomenys4.txt");
-
-			auto finish4 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed4 = finish4 - start4;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed4.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start5 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_DEKAI_Antra_Strategija("Duomenys5.txt");
-
-			auto finish5 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed5 = finish5 - start5;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed5.count() << " s\n";
-			} // DEKU REZ
-			{std::ofstream fr("LISTAI_REZULTATAI.txt");
-			auto start1 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Antra_Strategija("Duomenys1.txt");
-
-			auto finish1 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed1 = finish1 - start1;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed1.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start2 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Antra_Strategija("Duomenys2.txt");
-
-			auto finish2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed2 = finish2 - start2;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed2.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start3 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Antra_Strategija("Duomenys3.txt");
-
-			auto finish3 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed3 = finish3 - start3;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed3.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start4 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Antra_Strategija("Duomenys4.txt");
-
-			auto finish4 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed4 = finish4 - start4;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed4.count() << " s\n";
-			/////////////////////////////////////////////////////////////////////////////////////
-			auto start5 = std::chrono::high_resolution_clock::now();
-
-			InputOutput_LISTAI_Antra_Strategija("Duomenys5.txt");
-
-			auto finish5 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed5 = finish5 - start5;
-			fr << "Programos vykdymo laikas su vektoriais: " << elapsed5.count() << " s\n";
-			} // LISTU REZ
+			system("pause");
 		}
+		
 		else
 		{
-			std::cout << "SKAICIU (1) Jei norite nuskaityti 1 faila" << std::endl;
-			std::cout << "SKAICIU (5) Jei norite nuskaityti visus failus" << std::endl;
+			std::cout << "SKAICIU (1) Jei norite nuskaityti 1 faila naudojant algorimus" << std::endl;
+			std::cout << "SKAICIU (2) Jei norite nuskaityti 1 faila be algoritmu" << std::endl;
 			goto taskas2;
 		}
 	}
